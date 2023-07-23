fb6a19ebfb00872890bb401b2001f89167d57f7e
Jason Miranda <127341756+Son2641@users.noreply.github.com>
2023-07-23 21:53:05 +0800

    fix

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+app.use(cors({ origin: process.env.VUE_URL }));
 const PORT = process.env.PORT || 3000;
 
 app.set('port', PORT);
--- frontend/services/studentService.js
+++ frontend/services/studentService.js
@@ -1,28 +1,29 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8001/api/v1';
+// const API_BASE_URL = 'http://localhost:8001/api/v1';
+const API_BASE_URL = 'https://student-list-ckq3.onrender.com';
 
 export default {
   createStudent(studentData) {
-    return axios.post(`${API_BASE_URL}/student`, studentData);
+    return axios.post(`${API_BASE_URL}/api/v1/student`, studentData);
   },
 
   getAllStudents() {
-    return axios.get(`${API_BASE_URL}/students`);
+    return axios.get(`${API_BASE_URL}/api/v1/students`);
   },
 
   getStudent(studentId) {
-    return axios.get(`${API_BASE_URL}/students/${studentId}/update`);
+    return axios.get(`${API_BASE_URL}/api/v1/students/${studentId}/update`);
   },
 
   updateStudent(studentId, studentData) {
     return axios.put(
-      `${API_BASE_URL}/students/${studentId}/update`,
+      `${API_BASE_URL}/api/v1/students/${studentId}/update`,
       studentData
     );
   },
 
   deleteStudent(studentId) {
-    return axios.delete(`${API_BASE_URL}/students/${studentId}`);
+    return axios.delete(`${API_BASE_URL}/api/v1/students/${studentId}`);
   },
 };

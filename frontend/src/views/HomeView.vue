5104933ad78bc27c3939d5f115bc3cc3b764c5c6
Jason Miranda <127341756+Son2641@users.noreply.github.com>
2023-07-23 22:24:54 +0800

    fix

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <h1 class="display-4">Welcome to Student Lists</h1>
       <p class="lead">
-        This is a simple Student app where you can add, update and delete the list of your students.
+        This is a simple CRUD app, take note all data can be accessed by all users logged in. 
       </p>
       <p class="lead">
         Login to get started.

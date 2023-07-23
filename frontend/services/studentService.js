6305494e219bd7e108313d81f248c2d354266ac5
Jason Miranda <127341756+Son2641@users.noreply.github.com>
2023-07-23 21:58:36 +0800

    fix

--- frontend/services/studentService.js
+++ frontend/services/studentService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:8001/api/v1';
-const API_BASE_URL = 'https://student-list-ckq3.onrender.com';
+const API_BASE_URL = 'https://studentlist-api.onrender.com';
 
 export default {
   createStudent(studentData) {
